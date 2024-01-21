1ae037887a46dfd7a32d465c789a694d765256c8
최정윤 <ej921004@naver.com>
2024-01-22 00:08:08 +0900

    [최정윤] chap01 - [PGS] 12906 : 같은 숫자는 싫어

--- Chap01-Data-Structure/[PGS]12906/12906_choijy.js
+++ Chap01-Data-Structure/[PGS]12906/12906_choijy.js
@@ -1,1 +1,15 @@
-// 여기에 문제 코드 올리기 - 정윤
+// 스택/큐 > 같은 숫자는 싫어
+// school.programmers.co.kr/learn/courses/30/lessons/12906
+https: function solution(arr) {
+  const result = [];
+
+  arr.forEach((item, idx) => {
+    if (idx === 0 || arr[idx - 1] !== item) result.push(item);
+  });
+
+  return result;
+}
+
+/* 아래는 테스트 출력용 */
+console.log(solution([1, 1, 3, 3, 0, 1, 1]));
+console.log(solution([4, 4, 4, 3, 3]));
