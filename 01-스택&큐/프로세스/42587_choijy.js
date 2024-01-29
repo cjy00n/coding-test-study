9bc8bc2321f4d33271580e07b27599d4cab9850e
최정윤 <ej921004@naver.com>
2024-01-29 10:45:55 +0900

    [최정윤] Chap01 - [PGS] 42587 : 프로세스

--- 01-스택&큐/프로세스/42587_choijy.js
+++ 01-스택&큐/프로세스/42587_choijy.js
@@ -1,7 +1,35 @@
 // 스택/큐 > 프로세스
 // https://school.programmers.co.kr/learn/courses/30/lessons/42587
 
+const returnMax = (arr) => {
+  let maxValue = 0;
+  arr.forEach((p) => {
+    if (p[0] >= maxValue) {
+      maxValue = p[0];
+    }
+  });
+  return maxValue;
+};
+
 function solution(priorities, location) {
+  let arr = priorities.map((p, i) => [p, i]);
+  let order = 0;
+
+  while (true) {
+    let maxValue = returnMax(arr);
+    let temp = arr.shift();
+
+    if (temp[0] !== maxValue) arr.push(temp);
+    else {
+      order++;
+      if (temp[1] === location) break;
+    }
+  }
+
+  return order;
+}
+
+function otherSolution(priorities, location) {
   let arr = priorities.map((p, i) => [p, i]);
   let order = 0;
 
